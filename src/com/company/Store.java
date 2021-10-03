b897a88baf17ca8b17cbf9f3f2e53b29472f76ce
dpacker123 <89939888+dpacker123@users.noreply.github.com>
2021-10-03 15:28:17 -0400

    store is looking better

--- src/com/company/Store.java
+++ src/com/company/Store.java
@@ -22,6 +22,7 @@ public class Store {
 
 
   public static void main(String[] args) throws IOException {
+       // reading customer.txt into the customers array list (does not work)
       var filename = "Customers.txt";
       var getFile = Paths.get(filename);
       ArrayList<List<String>> Customers  = new ArrayList<>();
@@ -31,12 +32,12 @@ public class Store {
 
   }
    public void runStore(){
-      var userInput = new Scanner(System.in);
-      System.out.println("");
+      var userInput = new Scanner(System.in);// creates scanner for user input
+      System.out.println(""); // had to put this in so the code works :(
       while(true){
          Menu();
-         var userChoice = userInput.nextInt();
-         switch (userChoice){
+         var userChoice = userInput.nextInt(); // assigns user input to a variable, int because it is 1 -3.
+         switch (userChoice){ //creates a switch that goes through the cases, still having issues
             case 1:
                addCustomer(userInput);
             case 2:
@@ -51,18 +52,18 @@ public class Store {
    }
 
    private void selectCustomer(Scanner userInput) {
-      userInput.nextLine();
+      userInput.nextLine(); // eats \n from previous call to next int
       System.out.println("Type the name of the customer you would like" +
               "to select");
-      var selectCust = userInput.nextLine();
-      System.out.println("The customer " +selectCust + " Has been selected");
+      var selectCust = userInput.nextLine(); // saves user input as a new variable
+      System.out.println("The customer " +selectCust + " Has been selected"); // this breaks the program :(
 
 
    }
 
-   private void Menu() {
+   private void Menu() { //basic menu Self explanatory
       System.out.println("===========================================");
-      System.out.println("" + Customers );
+      System.out.println("" + Customers );// test to make sure customer list is being updated
       System.out.println("What would you like to do?");
       System.out.println("     [1] Add a new Customer");
       System.out.println("     [2] Select a Customer");
@@ -71,11 +72,11 @@ public class Store {
    }
 
    private void addCustomer(Scanner userInput) {
-      userInput.nextLine();
+      userInput.nextLine(); // eats \n from previous from previous call
       System.out.println("Please type the name of the " +
               "customer you wish to add");
-      var newCustomer = userInput.nextLine();
-      Customers.add(newCustomer);
+      var newCustomer = userInput.nextLine();//saves user input as new variabe
+      Customers.add(newCustomer); // adds users input to customers array list
       System.out.println("Thank you for adding " +newCustomer+ " to the list");
 
 
